c1a54ecf0cdffd72092377631bca33097efafec0
huangs <huangs@gidomino.com>
2020-03-11 17:49:48 +0800

    re

--- src/utils/http.js
+++ src/utils/http.js
@@ -218,9 +218,9 @@ if (process.env.NODE_ENV == 'development') {
   shupingURL = `${protocol}//192.168.100.8:8080`
   // localURL = `${protocol}//192.168.100.17:8080`
   // localURL = `${protocol}//iot.gidomino.com`
-  // localURL = `${protocol}//192.168.100.8:8080`
+  localURL = `${protocol}//192.168.100.8:8080`
   // localURL = `${protocol}//192.168.100.89:8080`
-  localURL = `${protocol}//lorns.gidomino.com`
+  // localURL = `${protocol}//lorns.gidomino.com`
   // localURL = `${protocol}//www.ewtes.com`
 } else if (process.env.NODE_ENV == 'production') {
   //  localURL = iot = devURL = xiaohuURL = shupingURL = `${protocol}//192.168.100.3`
